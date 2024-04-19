d13229aaadd9ce058a7a8a15aad3fba3b5399782
Souhaib-789 <souhaibsouhaib838@gmail.com>
2024-04-19 17:06:40 +0500

    Splash Screen

--- android/app/src/main/java/com/healthease/MainActivity.java
+++ android/app/src/main/java/com/healthease/MainActivity.java
@@ -4,10 +4,18 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
-// import org.devio.rn.splashscreen.SplashScreen;
-// import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
+import android.os.Bundle;
+
+
 public class MainActivity extends ReactActivity {
 
+
+  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
+    }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
@@ -16,13 +24,6 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "HealthEase";
   }
-
-// @Override
-//     protected void onCreate(Bundle savedInstanceState) {
-//         SplashScreen.show(this);  // here
-//         super.onCreate(savedInstanceState);
-//     }
-
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
    * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -1,7 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React, { useEffect } from 'react';
 import AuthStack from './AuthStack';
-// import SplashScreen from 'react-native-splash-screen';
+import SplashScreen from 'react-native-splash-screen';
 import { Text, View, Modal as RNModal, ActivityIndicator, Platform, SafeAreaView } from 'react-native';
 import AppStack from './AppStack';
 import { useDispatch, useSelector } from 'react-redux';
@@ -95,20 +95,15 @@ const AppNavigation = () => {
     } else {
       dispatch(login(false));
     }
-    // SplashScreen.hide();
+    SplashScreen.hide();
   };
 
 
   return (
     <NavigationContainer>
 
-      {
-        islogin ? (
-          <AppStack />
-        ) : (
-          <AuthStack />)
-      }
-      {/* {Platform.OS === 'ios' ? (
+
+      {Platform.OS === 'ios' ? (
         <SafeAreaView style={{ flex: 1 }}>
           {islogin == undefined ? (
             SplashScreen.show()
@@ -124,7 +119,7 @@ const AppNavigation = () => {
         <AppStack />
       ) : (
         <AuthStack />
-      )}  */}
+      )}
 
       <RNModal visible={loading} transparent>
         <View style={{ flex: 1, backgroundColor: 'rgba(0,0,0,0.5)', justifyContent: 'center', alignItems: 'center' }}>
